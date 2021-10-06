7e71fd3e6adfdf0bdb1e6d3db579f390d7619690
jennifergoldberg <jagoldbe@gmail.com>
2021-10-06 15:11:51 -0400

    added scroll to top arrow - need to fix

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+// Nav Links Toggle
+
 const navToggle = document.querySelector('.nav-toggle');
 
 const navLinks = document.querySelectorAll('.nav__link');
@@ -11,3 +13,36 @@ navLinks.forEach(link => {
     document.body.classList.remove('nav-open');
   })
 });
+
+
+// Scroll to Top Button
+// TODO Fix This!
+const target = document.querySelector('footer');
+
+const scrollTopBtn = document.querySelector('.scroll-to-top');
+
+const rootElement = document.documentElement;
+
+function callback(entries, observer) {
+  console.log(observer);
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      scrollTopBtn.classList.add('show-btn');
+    } else {
+      scrollTopBtn.classList.remove('show-btn');
+    }
+  })
+}
+
+function scrollToTop() {
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+
+scrollTopBtn.addEventListener('click', scrollToTop);
+
+let observer = new IntersectionObserver(callback);
+
+observer.observe(target);
